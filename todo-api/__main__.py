64244aa4f9218d638eeb2142fa3478c585496b7d
Eryk Mika <erykmika108@gmail.com>
2024-05-02 12:15:07 +0200

    Change hardcoded mapping to asdict

--- todo-api/__main__.py
+++ todo-api/__main__.py
@@ -5,6 +5,8 @@ from .model.base import session
 from .model.base import engine
 from .model.base import Base
 from .model.task import Task
+from datetime import datetime
+from dataclasses import asdict
 import uvicorn
 
 
@@ -12,6 +14,8 @@ Base.metadata.create_all(bind=engine, checkfirst=True)
 
 
 class ToDo(BaseModel):
+    """JSON POST todo content"""
+
     content: str
 
 
@@ -19,17 +23,26 @@ app = FastAPI()
 
 
 @app.get("/tasks")
-async def read_root():
+async def get_tasks():
+    """Get all tasks"""
     task_entities = session.execute(select(Task).order_by(Task.id))
     tasks = {}
     for entity in task_entities.scalars().fetchall():
-        tasks[entity.id] = {"content": entity.content, "timestamp": entity.timestamp}
+        tasks[entity.id] = asdict(entity)
     return tasks
+
+
+@app.get("/tasks/{task_id}")
+async def get_task(task_id: int):
+    """Get the task with the given id"""
+    chosen_entity = session.execute(select(Task).where(Task.id == task_id)).scalar_one()
+    return asdict(chosen_entity)
 
 
 @app.post("/task")
 async def add_task(task: ToDo):
-    session.add(Task(task.content))
+    """Add a new task"""
+    session.add(Task(content=task.content, timestamp=datetime.now().isoformat()))
     session.commit()
 
 
--- todo-api/model/task.py
+++ todo-api/model/task.py
@@ -2,15 +2,13 @@ from datetime import datetime
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from .base import Base
+from dataclasses import dataclass
 
 
+@dataclass
 class Task(Base):
     __tablename__ = "Task"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     content: Mapped[str] = mapped_column(nullable=False)
     timestamp: Mapped[str] = mapped_column(nullable=False)
-
-    def __init__(self, content: str):
-        self.content = content
-        self.timestamp = datetime.now().isoformat()
